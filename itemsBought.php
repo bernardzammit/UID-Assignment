8738be5346b45aaf51d020e1849ecfd91de87284
shecam13 <sheryl13c@gmail.com>
2014-11-27 09:29:13 +0100

    commit test
    ggg

---
+++ itemsBought.php
@@ -0,0 +1,30 @@
+<?php
+	// call header file
+	include 'userDetails.php'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	
+	<head>	
+		<link rel = "stylesheet" href = "css/bootstrap.min.css">
+		<link rel = "stylesheet" href = "css/bootstrap.css" media= "screen"> 
+		
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+	</head>
+	
+	<body>
+		<div class="container">
+		
+	
+				<div class="col-lg-8">
+					<p>Helllooooooooooooo</p>
+				</div>
+		
+		
+		</div>
+	
+	
+	</body>
+</html>
