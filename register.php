87061beb3c044b47cc1a271c6a6dd60bd86873bf
natashanikolic <natashanikolic@users.noreply.github.com>
2014-11-27 14:22:05 +0100

    register updated changes
    changes discussed in meeting

--- register.php
+++ register.php
@@ -27,119 +27,121 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-12" "col-xs-6">
-					<form class="form-horizontal">
-					  <fieldset>
-					    <legend>Personal Details</legend>
-					    <div class="form-group">
-					      <label for="Name" class="col-lg-4 col-xs-4 control-label">Name</label>
-					      <div class="col-lg-3 col-xs-3">
-					      	<span class="nameFirst">
-					      		<input type="text" class="form-control" id="FName" placeholder= "First">
-					      	</span>
-					      </div>
-					      <div class="col-lg-3 col-xs-3">
-					      	<span class="nameLast">
-					      		<input type="text" class="form-control" id="LName" placeholder="Last">
-					      	</span>
-					      </div>
-					    </div>
+					<div class="well bs-component">
+						<form class="form-horizontal">
+						  <fieldset>
+						    <legend>Personal Details</legend>
+						    <div class="form-group">
+						      <label for="Name" class="col-lg-4 col-xs-4 control-label">Name</label>
+						      <div class="col-lg-3 col-xs-3">
+						      	<span class="nameFirst">
+						      		<input type="text" class="form-control" id="FName" placeholder= "First">
+						      	</span>
+						      </div>
+						      <div class="col-lg-3 col-xs-3">
+						      	<span class="nameLast">
+						      		<input type="text" class="form-control" id="LName" placeholder="Last">
+						      	</span>
+						      </div>
+						    </div>
 
-					    <div class="form-group">
-					    	<label for="Gender" class="col-lg-4 col-xs-4 control-label">Gender</label>
-						    	<div class="col-lg-6 col-xs-6">
-							    	<div class="radio">
-		          						<label>
-		            						<input type="radio" name="optionsRadios" id="optionsRadios1" value="m">Male</label>
+						    <div class="form-group">
+						    	<label for="Gender" class="col-lg-4 col-xs-4 control-label">Gender</label>
+							    	<div class="col-lg-6 col-xs-6">
+								    	<div class="radio">
+			          						<label>
+			            						<input type="radio" name="optionsRadios" id="optionsRadios1" value="m">Male</label>
+			        					</div>
+			        					<div class="radio">
+			          						<label>
+			            						<input type="radio" name="optionsRadios" id="optionsRadios2" value="f">Female</label>
+			        					</div>
 		        					</div>
-		        					<div class="radio">
-		          						<label>
-		            						<input type="radio" name="optionsRadios" id="optionsRadios2" value="f">Female</label>
-		        					</div>
-	        					</div>
-        				</div>
+	        				</div>
 
-					    <div class="form-group">
-					    	<label for="inputAddress" class="col-lg-4 col-xs-4 control-label">Address</label>
-	  						<div class="col-lg-6 col-xs-6">
-	  							<textarea class="form-control" rows="3" id="inputAddress"></textarea>
-	  						</div>
-						</div>
+						    <div class="form-group">
+						    	<label for="inputAddress" class="col-lg-4 col-xs-4 control-label">Address</label>
+		  						<div class="col-lg-6 col-xs-6">
+		  							<textarea class="form-control" rows="3" id="inputAddress"></textarea>
+		  						</div>
+							</div>
 
-					    <div class="form-group">
-					      <label for="inputTown" class="col-lg-4 col-xs-4 control-label">Town</label>
-					      <div class="col-lg-6 col-xs-6">
-					        <input type="text" class="form-control" id="inputTown">
-					      </div>
-					    </div>
+						    <div class="form-group">
+						      <label for="inputTown" class="col-lg-4 col-xs-4 control-label">Town</label>
+						      <div class="col-lg-6 col-xs-6">
+						        <input type="text" class="form-control" id="inputTown">
+						      </div>
+						    </div>
 
-					    <div class="form-group">
-					      <label for="inputPostcode" class="col-lg-4 col-xs-4 control-label">Postcode</label>
-					      <div class="col-lg-6 col-xs-6">
-					        <input type="text" class="form-control" id="inputPostcode">
-					      </div>
-					    </div>
+						    <div class="form-group">
+						      <label for="inputPostcode" class="col-lg-4 col-xs-4 control-label">Postcode</label>
+						      <div class="col-lg-6 col-xs-6">
+						        <input type="text" class="form-control" id="inputPostcode">
+						      </div>
+						    </div>
 
-					    <div class="form-group">
-					      <label for="inputMobile" class="col-lg-4 col-xs-4 control-label">Mobile</label>
-					      <div class="col-lg-6 col-xs-6">
-					        <input type="tel" class="form-control" id="inputMobile">
-					      </div>
-					    </div>
+						    <div class="form-group">
+						      <label for="inputMobile" class="col-lg-4 col-xs-4 control-label">Mobile</label>
+						      <div class="col-lg-6 col-xs-6">
+						        <input type="tel" class="form-control" id="inputMobile">
+						      </div>
+						    </div>
 
-					    <div class="form-group">
-					      <label for="inputPhone" class="col-lg-4 col-xs-4 control-label">Phone</label>
-					      <div class="col-lg-6 col-xs-6">
-					        <input type="tel" class="form-control" id="inputPhone">
-					      </div>
-					    </div>
+						    <div class="form-group">
+						      <label for="inputPhone" class="col-lg-4 col-xs-4 control-label">Phone</label>
+						      <div class="col-lg-6 col-xs-6">
+						        <input type="tel" class="form-control" id="inputPhone">
+						      </div>
+						    </div>
 
-					    <div class="form-group">
-					      <label for="inputDOB" class="col-lg-4 col-xs-4 control-label">Date of Birth</label>
-					      <div class="col-lg-6 col-xs-6">
-					      		<input type="text" class="form-control" name="date" placeholder="mm-dd-yyyy" id="datepicker">					      		
-					      		
-					      </div>
-					    </div>
+						    <div class="form-group">
+						      <label for="inputDOB" class="col-lg-4 col-xs-4 control-label">Date of Birth</label>
+						      <div class="col-lg-6 col-xs-6">
+						      		<input type="text" class="form-control" name="date" placeholder="mm-dd-yyyy" id="datepicker">					      		
+						      		
+						      </div>
+						    </div>
 
-					    <legend>Username & Password</legend>
-					    <div class="form-group">
-					      <label for="inputUsername" class="col-lg-4 col-xs-4 control-label">Username</label>
-					      <div class="col-lg-6 col-xs-6">
-					        <input type="text" class="form-control" id="inputUsername">
-					      </div>
-					    </div>
+						    <legend>Username & Password</legend>
+						    <div class="form-group">
+						      <label for="inputUsername" class="col-lg-4 col-xs-4 control-label">Username</label>
+						      <div class="col-lg-6 col-xs-6">
+						        <input type="text" class="form-control" id="inputUsername">
+						      </div>
+						    </div>
 
-					    <div class="form-group">
-					      <label for="inputPassword" class="col-lg-4 col-xs-4 control-label">Password</label>
-					      <div class="col-lg-6 col-xs-6">
-					        <input type="password" class="form-control" id="inputPassword">
-					      </div>
-					    </div>
+						    <div class="form-group">
+						      <label for="inputPassword" class="col-lg-4 col-xs-4 control-label">Password</label>
+						      <div class="col-lg-6 col-xs-6">
+						        <input type="password" class="form-control" id="inputPassword">
+						      </div>
+						    </div>
 
-					    <div class="form-group">
-						    <div class="checkbox">
-	          					<label for="textArea" class="col-lg-4 col-xs-4 control-label"> </label>
-	          					<label>
-	            					<input type="checkbox"> I confirm that I have read and accept the Website Terms of Use
-	          					</label>
-	       					 </div>
-	       					 <div class="checkbox">
-	       					 	<label for="textArea" class="col-lg-4 col-xs-4 control-label"> </label>
-	          					<label>
-	            					<input type="checkbox"> I would like to receive emails about listings and offers
-	          					</label>
-	       					 </div>
-	       				</div>
+						    <div class="form-group">
+							    <div class="checkbox">
+		          					<label for="textArea" class="col-lg-4 col-xs-4 control-label"> </label>
+		          					<label>
+		            					<input type="checkbox"> I confirm that I have read and accept the Website Terms of Use
+		          					</label>
+		       					 </div>
+		       					 <div class="checkbox">
+		       					 	<label for="textArea" class="col-lg-4 col-xs-4 control-label"> </label>
+		          					<label>
+		            					<input type="checkbox"> I would like to receive emails about listings and offers
+		          					</label>
+		       					 </div>
+		       				</div>
 
-					    
-					    <div class="form-group">
-					      <div class="col-lg-10 col-lg-offset-8">
-					        <button class="btn btn-default">Cancel</button>
-					        <button type="submit" class="btn btn-primary">Submit</button>
-					      </div>
-					    </div>
-					  </fieldset>
-					</form>
+						    
+						    <div class="form-group">
+						      <div class="col-lg-10 col-lg-offset-8">
+						        <button class="btn btn-default">Cancel</button>
+						        <button type="submit" class="btn btn-primary">Submit</button>
+						      </div>
+						    </div>
+						  </fieldset>
+						</form>
+					</div>
 				</div>
 			</div>	
 
