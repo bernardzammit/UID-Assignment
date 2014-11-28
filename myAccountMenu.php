4638e794d3963261be295e4b7be9710e11c86c65
natashanikolic <natashanikolic@users.noreply.github.com>
2014-11-28 11:47:19 +0100

    .

---
+++ myAccountMenu.php
@@ -0,0 +1,31 @@
+<?php
+	// call header file
+	include 'userDetails.php'; 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	
+	<head>	
+		<link rel = "stylesheet" href = "css/bootstrap.min.css">
+		<link rel = "stylesheet" href = "css/bootstrap.css" media= "screen"> 
+		
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+	</head>
+	
+	<body>
+		
+				<div class="container-fluid">
+					<div class="col-lg-8">
+						<a href="#" class="btn btn-primary btn-lg">Add New Item</a>
+						<a href="#" class="btn btn-primary btn-lg">Current Listings</a>
+						<a href="#" class="btn btn-primary btn-lg">Items Bought</a>
+						<a href="#" class="btn btn-primary btn-lg">Items Sold</a>
+					</div>
+				</div>
+			
+				
+	
+	</body>
+</html>
