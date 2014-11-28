66be77a4ff55feb28f6f4eb83918c70d8f7c762f
shecam13 <sheryl13c@gmail.com>
2014-11-28 11:13:26 +0100

    upload2

--- userDetails.php
+++ userDetails.php
@@ -16,59 +16,59 @@
 	
 	<body>
 		<div class="container">
-			
-			<!-- user's profile picture -->
-			<div class="row">
-				<div class="col-lg-4">
-					<span class="glyphicon glyphicon-user"></span>
+			<div class="row" id="userSide">
+				<!-- user's profile picture -->
+				<div class="row" >
+					<div class="col-lg-4">
+						<span class="glyphicon glyphicon-user"></span>
+					</div>
 				</div>
-			</div>
-			
-			<!--seperator between image and the panel with user details-->
-			<div class="row">
-				<div class="col-lg-4">
-					<br>
+				
+				<!--seperator between image and the panel with user details-->
+				<div class="row">
+					<div class="col-lg-4">
+						<br>
+					</div>
 				</div>
-			</div>
-		
-			<div class="row"> <!-- row containing the user details panel-->
-				<div class="col-lg-4">
-					<div class="panel panel-primary">
-						
-						<div class="panel-heading">
-							<div class="row">
-								<div class="col-lg-10">
-								<h3 class="panel-title">Welcome User!</h3>
-								</div>
-								<div class="col-lg-2">
-								<a href="#">
-								  <span class="glyphicon glyphicon-pencil"></span>
-								</a>
-								</div>									
-							</div>	
-						</div>
-
-						<div class="panel-body">	
-							<div class="row">
-								<div class="container-fluid">
-									<div class="col-lg-6">
-										<p>First name: </p>
-										<p>Last name: </p>
-										<p>Email: </p>
-										<p>Date of Birth: </p>
-										<p>Address: </p>
-										<p><br>Current Listings: </p>
-										<p>Items Sold: </p>
-										<p>Items Bought: </p>
+			
+				<div class="row"> <!-- row containing the user details panel-->
+					<div class="col-lg-4">
+						<div class="panel panel-primary">
+							
+							<div class="panel-heading">
+								<div class="row">
+									<div class="col-lg-10">
+									<h3 class="panel-title">Welcome User!</h3>
 									</div>
+									<div class="col-lg-2">
+									<a href="#">
+									  <span class="glyphicon glyphicon-pencil"></span>
+									</a>
+									</div>									
 								</div>	
-							</div>	
+							</div>
+
+							<div class="panel-body">	
+								<div class="row">
+									<div class="container-fluid">
+										<div class="col-lg-6">
+											<p>First name: </p>
+											<p>Last name: </p>
+											<p>Email: </p>
+											<p>Date of Birth: </p>
+											<p>Address: </p>
+											<p><br>Current Listings: </p>
+											<p>Items Sold: </p>
+											<p>Items Bought: </p>
+										</div>
+									</div>	
+								</div>	
+							</div>
+							
 						</div>
-						
 					</div>
 				</div>
 			</div>
-
 		</div>
 	</body>
 </html>
