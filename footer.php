f12234e152478bf386a5d5e029828c776471d68b
bernardzammit <bernardzammit93@gmail.com>
2014-11-29 11:21:06 +0100

    Header, footer and index
    Uploaded footer/header files and index which is going to be the home
    page.

---
+++ footer.php
@@ -0,0 +1,94 @@
+<head>
+    <link rel = "stylesheet" href = "css/bootstrap.min.css">
+    <link rel = "stylesheet" href = "css/bootstrap.css" media= "screen">
+    <link rel="stylesheet" href="css/newcss.css">
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <div class="container">
+        <div class="page-footer">
+            <div class="row"> <!-- contains subscribe and contact us -->
+                <div class="container-fluid">
+                    <div class="col-lg-6">
+                        <div class="panel panel-warning">
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Subscribe</h3>
+                            </div>
+                            <div class="panel-body">form, enter e-mail to receive newsletter</div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="panel panel-warning">
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Contact Us</h3>
+                            </div>
+                            <div class="panel-body">Contact us.. share on fb, twitter, google+...</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="container-fluid">
+                    <div class="col-lg-3">
+                        <div class="panel panel-warning">
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Payment Methods</h3>
+                            </div>
+                            <div class="panel-body">Contact Us<br />
+                                                    My Account<br />
+                                                    Start Fitness Blog<br />
+                                                    North East Runner<br />
+                                                    Price Guarantee<br />
+                                                    Discount Codes<br /></div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="panel panel-warning">
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Order Information</h3>
+                            </div>
+                            <div class="panel-body">Contact Us<br />
+                                                    My Account<br />
+                                                    Start Fitness Blog<br />
+                                                    North East Runner<br />
+                                                    Price Guarantee<br />
+                                                    Discount Codes<br /></div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="panel panel-warning">
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Customer Care</h3>
+                            </div>
+                            <div class="panel-body">Contact Us<br />
+                                                    My Account<br />
+                                                    Start Fitness Blog<br />
+                                                    North East Runner<br />
+                                                    Price Guarantee<br />
+                                                    Discount Codes<br /></div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="panel panel-warning">
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Company Information</h3>
+                            </div>
+                            <div class="panel-body">Contact Us<br />
+                                                    My Account<br />
+                                                    Start Fitness Blog<br />
+                                                    North East Runner<br />
+                                                    Price Guarantee<br />
+                                                    Discount Codes<br /></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
--- index.php
+++ index.php
@@ -1,19 +1,16 @@
 <?php
-	//session_start();
-	include 'header.php'; //contain all the codes that we will use in the webpage
-	//include 'php/footer.php';
-?>
+    include 'header.php';
+    ?>
+
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		
-
-	</head>
-	
-	<body>
-		
-	
-	
-	</body>
-	
+    <head>
+        <link rel="stylesheet" href="css/bootstrap.css" media="screen">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    </head>
+    
+    
+    <?php include 'footer.php'; ?>
 </html>
