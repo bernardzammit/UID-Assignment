0cd7c3fd93c2a4707d5bed4bdb523bc185e604e8
bernardzammit <bernardzammit93@gmail.com>
2014-12-09 12:40:55 +0100

    Homepage ready

---
+++ UID-new/item.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <!-- Bootstrap Core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Custom CSS -->
+        <link href="css/modern-business.css" rel="stylesheet">
+
+        <!-- Custom Fonts -->
+        <link href="font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+        <!-- Extra CSS -->
+        <link href="css/extracss.css" rel="stylesheet">
+        
+    </head>
+    
+    <body>
+        <div class="col-md-4 col-sm-6">
+            <div class="thumbnail">
+               <img class="img-responsive" src="images\adobe_software.jpg" width="130" height="85"  alt="">
+               <div class="caption">
+                    <h3>Adobe Acrobat X Standard<br>
+                        <small>For Windows</small>
+                    </h3>
+                    <p>The Acrobat X Standard Software for Windows from Adobe is a software suite that allows you to...</p>
+                    <ul class="list-inline">
+                        <li><a href="#"><i class="fa fa-2x fa-eye"></i></i></a>
+                        </li>
+                        <li><a href="#"><i class="fa fa-2x fa-shopping-cart"></i></a>
+                        </li>
+                        <li  id="price"><div class="col-sm-1"><font size="4">&euro;45</font></div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
