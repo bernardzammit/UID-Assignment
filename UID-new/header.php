ccf9fb2ffb0f6675372b173ca83a78c5a5aa1738
natashanikolic <natashanikolic@users.noreply.github.com>
2014-12-29 17:52:07 +0100

    cart index

--- UID-new/header.php
+++ UID-new/header.php
@@ -247,10 +247,10 @@
                     <div class="modal-content">
                         <div class="modal-header">
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                                <h4 class="modal-title" id="myModalLabel">Warning</h4>
+                                <h4 class="modal-title" id="myModalLabel">You must log in below or register to view cart!</h4>
                         </div>
                     <div class="modal-body">
-                        You must log in below or register to view cart!
+                        
                          <form class="form-horizontal">
                                             <fieldset>
                                                 <!-- <legend>Log In</legend> -->
