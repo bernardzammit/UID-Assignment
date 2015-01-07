c688988406562b9e838fe667b4faa9b96132dc99
natashanikolic <natashanikolic@users.noreply.github.com>
2015-01-07 13:48:08 +0100

    modals

--- UID-new/viewItem.php
+++ UID-new/viewItem.php
@@ -132,10 +132,28 @@
                                 </a>
                                 <a href="#">
                                     <h4>
-                                        <span class="fa fa-flag"> Report Item</span><br>
+                                        <span class="fa fa-flag" data-toggle="modal" data-target="#myModal1"> Report Item</span><br>
                                     </h4>
                                  </a>    
                             </div>
+                             <!-- Modal -->
+                                <div class="modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                                  <div class="modal-dialog">
+                                    <div class="modal-content">
+                                      <div class="modal-header">
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                        <h4 class="modal-title" id="myModalLabel">Remove Item</h4>
+                                      </div>
+                                      <div class="modal-body">
+                                        Are you sure you want to report this item? 
+                                      </div>
+                                      <div class="modal-footer">
+                                        <button type="button" class="btn btn-default" data-dismiss="modal">No</button>
+                                        <button type="button" class="btn btn-primary">Yes</button>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
                             <br>
 
                         </div>
